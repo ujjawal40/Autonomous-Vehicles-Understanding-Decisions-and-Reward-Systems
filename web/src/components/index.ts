efe068a3d4326a98157c128247368bc5175cfe09
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2026-01-27 18:22:17 -0500

    Add component barrel exports

---
+++ web/src/components/index.ts
@@ -0,0 +1,6 @@
+export { Header } from './Header';
+export { HighwayCanvas } from './HighwayCanvas';
+export { DecisionFeed } from './DecisionFeed';
+export { RewardBreakdown } from './RewardBreakdown';
+export { ActionProbabilities } from './ActionProbabilities';
+export { AgentTooltip } from './AgentTooltip';
