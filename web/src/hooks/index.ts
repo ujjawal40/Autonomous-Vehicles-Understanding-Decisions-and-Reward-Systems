6ca37a41dd7fd9bfa34fe6d54858038ae2163570
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2026-01-28 03:49:55 -0500

    Export useTrainingWebSocket from hooks index

--- web/src/hooks/index.ts
+++ web/src/hooks/index.ts
@@ -7,9 +7,11 @@
 export { useSimulation } from './useSimulation';
 export { useLondonSimulation } from './useLondonSimulation';
 export { useTrainingAPI } from './useTrainingAPI';
+export { useTrainingWebSocket } from './useTrainingWebSocket';
 export type {
   TrainingConfig,
   TrainingRun,
   RewardPreset,
   AlgorithmConfig,
 } from './useTrainingAPI';
+export type { TrainingState, TrainingUpdate } from './useTrainingWebSocket';
