b35af692d1112c4ad88f8758bdf41981c9ff57fd
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2026-01-28 03:46:58 -0500

    Add hooks index with all exports
    Exports:
    - useSimulation: Highway environment WebSocket
    - useLondonSimulation: London navigation WebSocket
    - useTrainingAPI: REST API interactions
    - Type exports for TrainingConfig, TrainingRun, etc.

---
+++ web/src/hooks/index.ts
@@ -0,0 +1,15 @@
+/**
+ * React Hooks
+ *
+ * Custom hooks for the Autonomous Decision Visualizer.
+ */
+
+export { useSimulation } from './useSimulation';
+export { useLondonSimulation } from './useLondonSimulation';
+export { useTrainingAPI } from './useTrainingAPI';
+export type {
+  TrainingConfig,
+  TrainingRun,
+  RewardPreset,
+  AlgorithmConfig,
+} from './useTrainingAPI';
