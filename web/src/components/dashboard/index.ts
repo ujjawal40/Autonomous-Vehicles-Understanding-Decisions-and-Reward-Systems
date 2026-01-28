19a91157a31c5766d71f9abf02a6269485446946
ujjawal40 <157764485+ujjawal40@users.noreply.github.com>
2026-01-28 03:34:17 -0500

    Update dashboard index with all component exports
    Exports:
    - Dashboard (main page)
    - DashboardHeader
    - TrainingControls
    - RewardEditor + DEFAULT_CODE + RewardConfig type
    - ProbabilityVolcano
    - RiskGauge
    - MetricsPanel
    - TrainingHistory

---
+++ web/src/components/dashboard/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Dashboard Components
+ *
+ * SpaceX-inspired training dashboard for the Autonomous Decision Visualizer.
+ */
+
+export { Dashboard } from './Dashboard';
+export { DashboardHeader } from './DashboardHeader';
+export { TrainingControls } from './TrainingControls';
+export { RewardEditor, DEFAULT_CODE } from './RewardEditor';
+export type { RewardConfig } from './RewardEditor';
+export { ProbabilityVolcano } from './ProbabilityVolcano';
+export { RiskGauge } from './RiskGauge';
+export { MetricsPanel } from './MetricsPanel';
+export { TrainingHistory } from './TrainingHistory';
